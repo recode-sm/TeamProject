89ed27e3da15c61035667934a7b27f1397349d73
yuer2009 <yuer2009@naver.com>
2022-06-30 13:48:55 +0900

    베이스

--- team/src/main/java/com/team/dao/FieldDAO.java
+++
@@ -1,8 +0,0 @@
-package com.team.dao;
-
-import com.team.domain.FieldDTO;
-
-public interface FieldDAO {
-
-	public void insertFeild(FieldDTO fieldDTO);
-}
--- team/src/main/java/com/team/service/FieldService.java
+++
@@ -1,9 +0,0 @@
-package com.team.service;
-
-import com.team.domain.FieldDTO;
-
-public interface FieldService {
-		
-	public void insertFeild(FieldDTO fieldDTO);
-		
-}

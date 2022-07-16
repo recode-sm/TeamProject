253ab1175860c08001a5d72414d64aafd76d6a4c
jy1915 <65592247+jy1915@users.noreply.github.com>
2022-07-16 19:00:33 +0900

    이메일 인증 컨트롤러 추가 (#114)
    * 예약확인
    
    예약확인페이지
    
    * 예약확인페이지 수정
    
    예약확인페이지 수정
    
    * 헤더 수정
    
    header
    회원관련부분 링크 수정
    
    reservation 예약확인 링크 수정
    
    * 예약확인 컨트롤러
    
    에약확인 컨트롤러
    예약확인 비밀번호 체크 jsp파일
    
    * 예약확인  페이지
    
    예약여부 추가
    
    * 예약확인
    
    로그인 필수
    
    * reservMapper,jsp 수정
    
    reservMapper,jsp 수정
    
    * 구장 선택 알림창
    
    구장 필수 선택 알림창
    
    * Update reservationMapper.xml
    
    reservation 매퍼 수정
    
    * Update reservSelect.jsp
    
    * 전체페이지 title 수정
    
    title 수정
    예약 login_msg 추가
    
    * 로그인체크 컨트롤러에추가
    
    로그인체크 컨트롤러에추가
    
    * 로그인,비밀번호
    
    엔터키 추가
    
    * 아이디저장,css추가
    
    아이디저장,css추가
    
    * 로그인 아이디저장 수정
    
    로그인 아이디저장 수정
    
    * 로그인,회원 css
    
    로그인 아이디 저장 ,회원 css
    
    * 게시판 qna css수정
    
    게시판 qna css수정
    
    * insert 수정, mapper수정
    
    insert 수정, date mapper수정
    
    * 통합본 수정
    
    qna
    QuestionController list 수정
    
    * loginForm.jsp
    
    로그인 폼 수정
    
    * Create MailSendController.java
    
    이메일 인증 컨트롤러 추가

---
+++ team/src/main/java/com/team/controller/MailSendController.java
@@ -0,0 +1,48 @@
+package com.team.controller;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MailSendController {
+
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@RequestMapping(value = "member/mailCheck", method = RequestMethod.GET)
+	@ResponseBody
+	public String mailCheck(@RequestParam("email") String email) throws Exception{
+	    int serti = (int)((Math.random()* (99999 - 10000 + 1)) + 10000);
+	    
+	    String from = "rlawodyd33@naver.com";//보내는 이 메일주소
+	    String to = email;
+	    String title = "회원가입시 필요한 인증번호 입니다.";
+	    String content = "[인증번호] "+ serti +" 입니다. <br/> 인증번호 확인란에 기입해주십시오.";
+	    String num = "";
+	    try {
+	    	MimeMessage mail = mailSender.createMimeMessage();
+	        MimeMessageHelper mailHelper = new MimeMessageHelper(mail, true, "UTF-8");
+	        
+	        mailHelper.setFrom(from);
+	        mailHelper.setTo(to);
+	        mailHelper.setSubject(title);
+	        mailHelper.setText(content, true);       
+	        
+	        mailSender.send(mail);
+	        num = Integer.toString(serti);
+	        
+	    } catch(Exception e) {
+	        num = "error";
+	    }
+	    return num;
+	}
+}
